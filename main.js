e47bd67d136c59e246202e48aaf8e83e828e15c7
abdujalil-nizomiddinov <abdujalil2107@gmail.com>
2025-03-05 19:36:58 +0500

    shdbshdbs

---
+++ main.js
@@ -0,0 +1,49 @@
+// // Task-1
+// let gradus = parseInt(prompt("Gradusni kiriting:"));
+
+// if (gradus <= -20) {
+//   alert("Qahraton");
+// } else if (gradus > -20 && gradus <= 0) {
+//   alert("Juda sovuq");
+// } else if (gradus > 0 && gradus <= 10) {
+//   alert("Havo sovuq");
+// } else if (gradus > 10 && gradus <= 20) {
+//   alert("Salqin havo");
+// } else if (gradus > 20 && gradus <= 30) {
+//   alert("Iliq havo");
+// } else if (gradus > 30 && gradus <= 40) {
+//   alert("Issiq havo");
+// } else if (gradus >= 40) {
+//   alert("Jazirama");
+// }
+// // Task-1
+
+// // Task-2
+// let num = prompt("Son kiriting:");
+
+// if (num / 3 && num / 5) {
+//   alert("FizzBuzz");
+// } else if (num / 3) {
+//   alert("Fizz");
+// } else if (num / 5) {
+//   alert("Buzz");
+// } else {
+//   alert(num);
+// }
+// // Task-2
+
+// // Task-3
+// let num_1 = parseInt(prompt("Son-1 kiriting:"));
+// let num_2 = parseInt(prompt("Son-2 kiriting:"));
+// let amal = prompt("Amal kiriting:");
+
+// if (amal == "+") {
+//   alert("Natija:", num_1 + num_2);
+// } else if (amal == "-") {
+//   alert("Natija:", num_1 - num_2);
+// } else if (amal == ":") {
+//   alert("Natija:", num_1 / num_2);
+// } else {
+//   alert("Natija:", num_1 * num_2);
+// }
+// // Task-3
